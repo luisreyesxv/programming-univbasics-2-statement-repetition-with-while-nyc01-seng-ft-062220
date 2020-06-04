02b16eda03d72002b5f36e4047112364cf88280b
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-04 21:30:12 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -4,7 +4,8 @@ count_down = 0
 while count_down do
   if count_down == 0
     puts "Happy New Year!"
-    Break
-else puts count_down
+    break
+  else puts count_down
   count_down -=1
+  end
 end
